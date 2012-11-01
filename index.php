6287e468e344c53abdee83daa0cdfc99bdc7ca88
Claus35-DK <clih@itu.dk>
2012-11-01 13:45:05 +0100

    index.php links index.css

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!doctype html PUBLIC "-//W3C//DTD HTML+RDFa 1.1//EN">
 <html>
   <head>
-    <link rel="stylesheet" type="text/css" href="index.css"/>
+    <link rel="stylesheet" type="text/css" href="css/index.css"/>
   </head>
   <body>
     <p> some text </p>
