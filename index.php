1c44a42b2b55ae2ab73d6d8092480be2be4a5ff2
Claus35-DK <clih@itu.dk>
2012-11-01 12:29:22 +0100

    HTML initial

---
+++ index.php
@@ -0,0 +1,9 @@
+<!doctype html PUBLIC "-//W3C//DTD HTML+RDFa 1.1//EN">
+<html>
+  <head>
+    <link rel="stylesheet" type="text/css" href="index.css"/>
+  </head>
+  <body>
+    <p> some text </p>
+  </body>
+</html>
